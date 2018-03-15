98bfda0b390a49fbe3314a6df28c803cf5ca91dc
noyalin <1375162594@qq.com>
2018-03-15 19:27:17 +0800

    改

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var app = angular.module('myApp', ['ui.router']);
-app.config(["$stateProvider",  function ($stateProvider) {
+app.config(["$stateProvider", "$locationProvider", function ($stateProvider) {
+
     $stateProvider
         .state("sale", {//新零售
             url: '/sale',
@@ -17,5 +18,41 @@ app.config(["$stateProvider",  function ($stateProvider) {
             url: '/wechat',
             templateUrl:'other_page/wechat.html'
         })
+        .state("cms", {//cms
+            url: '/cms',
+            templateUrl:'other_page/cms.html'
+        })
+        .state("oms", {//cms
+            url: '/oms',
+            templateUrl:'other_page/oms.html'
+        })
+        .state("wms", {//cms
+            url: '/wms',
+            templateUrl:'other_page/wms.html'
+        })
+        .state("vms", {//cms
+            url: '/vms',
+            templateUrl:'other_page/vms.html'
+        })
+        .state("case_sale", {//行业案例-新零售
+            url: '/case_sale',
+            templateUrl:'other_page/case_sale.html'
+        })
+        .state("case_ec", {//行业案例-跨境电商
+            url: '/case_ec',
+            templateUrl:'other_page/case_ec.html'
+        })
+        .state("case_app", {//行业案例-app
+            url: '/case_app',
+            templateUrl:'other_page/case_app.html'
+        })
+        .state("case_wechat", {//行业案例-微信
+            url: '/case_wechat',
+            templateUrl:'other_page/case_wechat.html'
+        })
 }]);
 
+//去掉url中的感叹号
+app.config(['$locationProvider',function($locationProvider){
+    $locationProvider.hashPrefix("");
+}]);
