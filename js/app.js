de20b595578e839d953f8582ff45a6b6d5d3e147
noyalin <1375162594@qq.com>
2018-03-21 20:31:47 +0800

    绝对路径

--- js/app.js
+++ js/app.js
@@ -4,59 +4,59 @@ app.config(["$stateProvider", "$locationProvider", function ($stateProvider) {
     $stateProvider
         .state("sale", {//新零售
             url: '/solution/sale',
-            templateUrl:'other_page/sale.html'
+            templateUrl:'sale.html'
         })
         .state("ec", {//跨境电商
             url: '/solution/ec',
-            templateUrl:'other_page/ec.html'
+            templateUrl:'ec.html'
         })
         .state("app", {//app
             url: '/solution/app',
-            templateUrl:'other_page/app.html'
+            templateUrl:'app.html'
         })
         .state("wechat", {//微信
             url: '/solution/wechat',
-            templateUrl:'other_page/wechat.html'
+            templateUrl:'wechat.html'
         })
         .state("cms", {//cms
             url: '/product/cms',
-            templateUrl:'other_page/cms.html'
+            templateUrl:'cms.html'
         })
         .state("oms", {//cms
             url: '/product/oms',
-            templateUrl:'other_page/oms.html'
+            templateUrl:'oms.html'
         })
         .state("wms", {//cms
             url: '/product/wms',
-            templateUrl:'other_page/wms.html'
+            templateUrl:'wms.html'
         })
         .state("vms", {//cms
             url: '/product/vms',
-            templateUrl:'other_page/vms.html'
+            templateUrl:'vms.html'
         })
         .state("case_sale", {//行业案例-新零售
             url: '/case/sale',
-            templateUrl:'other_page/case_sale.html'
+            templateUrl:'case_sale.html'
         })
         .state("case_ec", {//行业案例-跨境电商
             url: '/case/ec',
-            templateUrl:'other_page/case_ec.html'
+            templateUrl:'case_ec.html'
         })
         .state("case_app", {//行业案例-app
             url: '/case/app',
-            templateUrl:'other_page/case_app.html'
+            templateUrl:'case_app.html'
         })
         .state("case_wechat", {//行业案例-微信
             url: '/case/wechat',
-            templateUrl:'other_page/case_wechat.html'
+            templateUrl:'case_wechat.html'
         })
         .state("introduce", {//行业案例-微信
             url: '/about/introduce',
-            templateUrl:'other_page/introduce.html'
+            templateUrl:'introduce.html'
         })
         .state("contact", {//行业案例-微信
             url: '/about/contact',
-            templateUrl:'other_page/contact.html'
+            templateUrl:'contact.html'
         })
 }]);
 
